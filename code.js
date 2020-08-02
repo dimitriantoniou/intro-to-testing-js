770cf93bd72bf4ef35ff0e64fdfea2124d6c2074
dimitriantoniou <dma2012@gmail.com>
2020-08-02 08:50:46 -0500

    Intro to testing js exercise 1

--- code.js
+++ code.js
@@ -1,4 +1,9 @@
 // helloWorld function 
-function helloWorld() {
+function helloWorld (){
     return "Hello, World!";
+}
+
+//sayHello function
+function sayHello(){
+    return "Hello";
 }
--- tests.js
+++ tests.js
@@ -1,15 +1,22 @@
 // Unit tests for the helloWorld function
 describe('helloWorld', function() {
     it('should be a defined function', function() {
-      expect(typeof helloWorld).toBe('function');
+        expect(typeof helloWorld).toBe('function');
     });
     it('should return a string when called', function() {
-      expect(typeof helloWorld()).toBe("string");
+        expect(typeof helloWorld()).toBe("string");
     });
     it('should return the string "Hello, World!" when executed', function() {
-      expect(helloWorld()).toBe("Hello, World!");
+        expect(helloWorld()).toBe("Hello, World!");
     });
     it("should never return 'undefined' when called", function() {
-      expect(helloWorld()).not.toBe(undefined);
+        expect(helloWorld()).not.toBe(undefined);
+    });
+});
+
+//Unit tests for sayHello function
+describe('sayHello', function() {
+    it('should be a defined function', function() {
+        expect(typeof sayHello).toBe('function');
     });
 });
